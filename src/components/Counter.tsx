3343b99195b98138bde9b2ef0077c09e9b7db5f0
ZHUaofei <2353536563@qq.com>
2020-07-17 15:32:25 +0800

    改成装饰器模式

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
-
+// import { Dispatch } from 'redux';
 import {addGUN, removeGUN} from '../redux/reducer';
 // import { StoreState } from '../types';
-
 
 // 创建类型接口
 export interface IProps {
@@ -12,9 +10,16 @@ export interface IProps {
     onIncrement: () => void,
     onDecrement: () => void
 }
-
+// @ts-ignore
+@connect(
+    (state: any) => ({ value: state.count }),
+    {
+     onIncrement: addGUN,
+     onDecrement: removeGUN
+    }
+)
 // 使用接口代替 PropTypes 进行类型校验
-class Counter extends React.PureComponent<IProps> {
+class Counter extends React.PureComponent {
     constructor(props:any) {
         super(props);
         this.state = {
@@ -22,18 +27,21 @@ class Counter extends React.PureComponent<IProps> {
             name: '21212'
         };
     }
+
     public render() {
-        const { onIncrement, onDecrement } = this.props;
-        console.log(this.props.value.countNumber, 'this.props');
+        console.log(this.props, 'this.props');
+        // @ts-ignore
+        const { value, onIncrement, onDecrement } = this.props;
+        // console.log(value, 'value')
         return (
             <p>
-                Clicked: { this.props.value.countNumber } times
+                点击次数: { value.countNumber }
                 <br/>
-                name: { this.props.value.name }
+                随机: { value.name }
                 <br />
                 <br />
-                <button onClick={ onIncrement } style={{ marginRight: 20 }}> +  </button>
-                <button onClick={ onDecrement }> - </button>
+                <button onClick={ onIncrement } style={{ marginRight: 20 }}> +  增加</button>
+                <button onClick={ onDecrement }> - 减少</button>
             </p>
         )
     }
@@ -42,27 +50,21 @@ class Counter extends React.PureComponent<IProps> {
 // 将 reducer 中的状态插入到组件的 props 中
 // 下面是单个reducer的时候，多个的时候需要选传入哪个reducer
 // const { test, count } = state
-const mapStateToProps = (state: any): { value: any } => ({
-    value: state.count
-});
-// const mapStateToProps = (state:any): { value: number } => ({
+// const mapStateToProps = (state: any): { value: any } => ({
 //     value: state.count
 // });
 
 // 将 对应action 插入到组件的 props 中
-// const mapDispatchToProps = (dispatch: Dispatch) => ({
-//     onDecrement: () => dispatch(removeGUN()),
-//     onIncrement: () => dispatch(addGUN())
-// });
-const mapDispatchToProps = (dispatch:Dispatch) => {
-    return {
-        onDecrement: () => {
-            dispatch(removeGUN())
-        },
-        onIncrement: () => {
-            dispatch(addGUN())
-        }
-    }
-}
+// const mapDispatchToProps = (dispatch:Dispatch) => {
+//     return {
+//         onDecrement: () => {
+//             dispatch(removeGUN())
+//         },
+//         onIncrement: () => {
+//             dispatch(addGUN())
+//         }
+//     }
+// }
 // 使用 connect 高阶组件对 Counter 进行包裹
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
